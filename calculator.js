89a01912902e9d3c3311f029e37faf5b5be62a7c
alrweashd <trkyabler@gmail.com>
2023-05-09 14:21:25 +0300

    solved

--- calculator.js
+++ calculator.js
@@ -1,6 +1,34 @@
 const prompt = require("prompt-sync")(); // Please don't touch me :)
 
-// The prompt method allows us to take an input from the user.
-// Run the code to see how to use prompt.
-const name = prompt("What's your name? ");
-console.log(`Hi, I'm ${name}.`);
+//let text1 = "Please enter the first number: ";
+let firstNumber = prompt("Please enter the first number: ");
+firstNumber = parseInt(firstNumber);
+//let text2 = "Please enter the second number: ";
+let secondNumber = prompt("Please enter the second number: ");
+secondNumber = parseInt(secondNumber);
+
+
+if (isNaN(firstNumber)||isNaN(secondNumber)){
+console.log("cannot be a string")
+//isNaN is not a number. return true if is a string
+
+}else{
+
+
+//let text3 = "Please choose the operation (+, -, /, *): ";
+const operation = prompt("Please choose the operation (+, -, /, *): ");
+switch(true){
+    case operation == "+":
+        console.log(`${firstNumber}+${secondNumber}=${firstNumber+secondNumber}`);
+        break;
+    case operation == "-":
+        console.log(`${firstNumber}-${secondNumber}=${firstNumber-secondNumber}`);
+        break;
+    case operation == "/":
+        console.log(`${firstNumber}/${secondNumber}=${firstNumber/secondNumber}`);
+        break;
+    case operation == "*":
+        console.log(`${firstNumber}*${secondNumber}=${firstNumber*secondNumber}`);
+        break;    
+}
+}
